625685778ed07d0c9612121096019647582c2e41
Omi Gibba <oumie.gibba@gmail.com>
2020-06-01 17:25:09 -0700

    Form styling, work in progress

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ function Footer(props) {
    return (
       <footer className="footer">
       <p>Site made by Omi Gibba in spring 2020</p>
-      <p>Contact me at: <a href="https://www.linkedin.com/in/omi-gibba">linkedin.com/in/omi-gibba</a></p>
+      <p><a href="https://www.linkedin.com/in/omi-gibba">Connect with me on LinkedIn</a></p>
       </footer>
   )
 }
